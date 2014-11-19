12d40d06d6d8f1eed10a51a43f310ea0683789ff
Topper Bowers <topper@toppingdesign.com>
2014-11-19 16:51:58 -0500

    refactor out a nice masthead class

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -1,30 +1,36 @@
 define([
-    'intern!object',
-    'intern/chai!assert',
-    'intern/dojo/node!leadfoot/helpers/pollUntil'
-], function (registerSuite, assert, pollUntil) {
+    'intern!bdd',
+    'intern/chai!expect',
+    'intern/dojo/node!leadfoot/helpers/pollUntil',
+    './support/masthead'
+], function (bdd, expect, pollUntil, Masthead) {
 
-    registerSuite({
-        'a test': function () {
-            return this.remote
-                .get('http://vitals.com')
-                .findById('q')
-                .click()
-                .type('abraham')
-                .end()
-                .findById('location_text')
-                .click()
-                .type('11215')
-                .end()
-                .findById('provider-go')
-                .click()
-                .then(pollUntil('return (/search/).test(window.location.path)', 5000))
-                .end()
-                .findByCssSelector('.searchsentence .q')
-                .getVisibleText()
-                .then(function (text) {
-                    assert.strictEqual(text, 'abraham');
+    with (bdd) {
+
+        describe('homepage', function () {
+            var subject, page;
+
+            beforeEach(function () {
+                page = this.remote.get('http://vitals.com')
+            });
+
+            describe('searching by doctor', function (){
+
+                beforeEach(function () {
+                  subject = new Masthead(page).searchFor('abraham', {near: 11215})
                 });
-        }
-    });
+
+                it('has abraham in the searchsentence', function () {
+                  return subject.findByCssSelector('.searchsentence .q')
+                      .getVisibleText()
+                      .then(function (text) {
+                          expect(text).to.equal('abraham')
+                      })
+                      .end()
+                });
+            });
+
+        })
+
+    }
 });
