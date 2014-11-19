e87adbc5743fe0446755b39d5d3d8b06f7586609
Topper Bowers <topper@toppingdesign.com>
2014-11-19 10:12:01 -0500

    a passing vitals.com test

--- tests/functional/index.js
+++ tests/functional/index.js
@@ -1,16 +1,29 @@
 define([
     'intern!object',
-    'intern/chai!assert'
-], function (registerSuite, assert) {
+    'intern/chai!assert',
+    'intern/dojo/node!leadfoot/helpers/pollUntil'
+], function (registerSuite, assert, pollUntil) {
 
     registerSuite({
         'a test': function () {
             return this.remote
-                .get('http://github.com')
-                .findByTagName('h1')
+                .get('http://vitals.com')
+                .findById('q')
+                .click()
+                .type('abraham')
+                .end()
+                .findById('location_text')
+                .click()
+                .type('11215')
+                .end()
+                .findById('provider-go')
+                .click()
+                .then(pollUntil('return (/search/).test(window.location.path)', 5000))
+                .end()
+                .findByCssSelector('.searchsentence .q')
                 .getVisibleText()
                 .then(function (text) {
-                    assert.strictEqual(text, 'Build software better, together.');
+                    assert.strictEqual(text, 'abraham');
                 });
         }
     });
